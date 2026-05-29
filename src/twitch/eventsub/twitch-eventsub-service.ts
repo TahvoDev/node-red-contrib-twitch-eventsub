3c6cd152c35fb88eaa48716566a7c478479c33f7
tahvo <tahvobiz@pm.me>
2026-05-28 19:02:58 -0500

    update sub processing

--- src/twitch/eventsub/twitch-eventsub-service.ts
+++ src/twitch/eventsub/twitch-eventsub-service.ts
@@ -5,16 +5,44 @@ import { AbstractNode } from '/@/AbstractNode';
 type SubscriptionHandler = (listener: EventSubWsListener, userId: string, cb: (event: any) => void) => void;
 
 const SUBSCRIPTION_HANDLERS: Record<string, SubscriptionHandler> = {
-  streamOnline:            (l, id, cb) => l.onStreamOnline(id, cb),
-  streamOffline:           (l, id, cb) => l.onStreamOffline(id, cb),
-  channelRedemptionAdd:    (l, id, cb) => l.onChannelRedemptionAdd(id, cb),
-  channelSubscription:     (l, id, cb) => l.onChannelSubscription(id, cb),
-  channelSubscriptionGift: (l, id, cb) => l.onChannelSubscriptionGift(id, cb),
-  channelCheer:            (l, id, cb) => l.onChannelCheer(id, cb),
-  channelRaidFrom:         (l, id, cb) => l.onChannelRaidFrom(id, cb),
-  channelRaidTo:           (l, id, cb) => l.onChannelRaidTo(id, cb),
-  channelFollow:           (l, id, cb) => l.onChannelFollow(id, id, cb),
-  channelChatMessage:      (l, id, cb) => l.onChannelChatMessage(id, id, cb),
+  // Stream
+  streamOnline:                 (l, id, cb) => l.onStreamOnline(id, cb),
+  streamOffline:                (l, id, cb) => l.onStreamOffline(id, cb),
+
+  // Channel
+  channelUpdate:                (l, id, cb) => l.onChannelUpdate(id, cb),
+  channelFollow:                (l, id, cb) => l.onChannelFollow(id, id, cb),
+  channelBan:                   (l, id, cb) => l.onChannelBan(id, cb),
+  channelUnban:                 (l, id, cb) => l.onChannelUnban(id, cb),
+  channelCheer:                 (l, id, cb) => l.onChannelCheer(id, cb),
+  channelRedemptionAdd:         (l, id, cb) => l.onChannelRedemptionAdd(id, cb),
+  channelSubscription:          (l, id, cb) => l.onChannelSubscription(id, cb),
+  channelSubscriptionGift:      (l, id, cb) => l.onChannelSubscriptionGift(id, cb),
+  channelRaidFrom:              (l, id, cb) => l.onChannelRaidFrom(id, cb),
+  channelRaidTo:                (l, id, cb) => l.onChannelRaidTo(id, cb),
+
+  // Chat
+  channelChatMessage:           (l, id, cb) => l.onChannelChatMessage(id, id, cb),
+
+  // Polls
+  channelPollBegin:             (l, id, cb) => l.onChannelPollBegin(id, cb),
+  channelPollProgress:          (l, id, cb) => l.onChannelPollProgress(id, cb),
+  channelPollEnd:               (l, id, cb) => l.onChannelPollEnd(id, cb),
+
+  // Predictions
+  channelPredictionBegin:       (l, id, cb) => l.onChannelPredictionBegin(id, cb),
+  channelPredictionProgress:    (l, id, cb) => l.onChannelPredictionProgress(id, cb),
+  channelPredictionLock:        (l, id, cb) => l.onChannelPredictionLock(id, cb),
+  channelPredictionEnd:         (l, id, cb) => l.onChannelPredictionEnd(id, cb),
+
+  // Hype Train
+  channelHypeTrainBegin:        (l, id, cb) => l.onChannelHypeTrainBegin(id, cb),
+  channelHypeTrainProgress:     (l, id, cb) => l.onChannelHypeTrainProgress(id, cb),
+  channelHypeTrainEnd:          (l, id, cb) => l.onChannelHypeTrainEnd(id, cb),
+
+  // Shoutouts
+  channelShoutoutCreate:        (l, id, cb) => l.onChannelShoutoutCreate(id, id, cb),
+  channelShoutoutReceive:       (l, id, cb) => l.onChannelShoutoutReceive(id, id, cb),
 };
 
 class TwitchEventsubService {
@@ -23,7 +51,6 @@ class TwitchEventsubService {
   userId: string;
   started = false;
 
-  // Track which subscription types are active and how many nodes want them
   private subscriptionCounts: Map<string, number> = new Map();
 
   onEventCb?: (event: any, subscriptionType: string) => void;
@@ -37,8 +64,6 @@ class TwitchEventsubService {
   addSubscription(type: string) {
     const count = this.subscriptionCounts.get(type) ?? 0;
     this.subscriptionCounts.set(type, count + 1);
-
-    // Only register with Twitch if this is the first node of this type
     if (count === 0 && this.started) {
       this.registerSubscription(type);
     }
@@ -66,11 +91,7 @@ class TwitchEventsubService {
   }
 
   async start(): Promise<void> {
-    // Register all currently requested subscription types
-    for (const type of this.subscriptionCounts.keys()) {
-      this.registerSubscription(type);
-    }
-
+    this.subscriptionCounts.forEach((_, type) => this.registerSubscription(type));
     this.node.log('EventSub WebSocket listener started');
     this.listener.start();
     this.started = true;
